6cc7eccf59dd1417bbd0c2bf2880029fb08a5186
roel.edeleon <roel.edeleon@gmail.com>
2024-02-14 20:21:52 +0800

    Commit 20240214 2021H
    Task 2 - Complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,7 +25,6 @@ gallery.addEventListener("click", (event) => {
   event.preventDefault();
   if (event.target.classList.contains("gallery__image")) {
     const imageUrl = event.target.getAttribute("data-source");
-    console.log(imageUrl);
     const instance = basicLightbox.create(
       `<img src="${imageUrl}" alt="image">`
     );
