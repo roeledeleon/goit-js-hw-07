3fd3540f294bd1004d4c8d460a06fc2ae38436c7
roel.edeleon <roel.edeleon@gmail.com>
2024-02-14 15:42:09 +0800

    Commit 20240214 1541H
    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  listItem.innerHTML = `
+      <a class="gallery__link" href="${item.original}">
+        <img 
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+        />
+      </a>
+    `;
+  gallery.appendChild(listItem);
+});
+
+// Event delegation to Open Modal
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    const imageUrl = event.target.dataset.source;
+    console.log(imageUrl);
+    const instance = basicLightBox.create(`img src="${imageUrl})" alt="image"`);
+    instance.show();
+  }
+});
 
 console.log(galleryItems);
